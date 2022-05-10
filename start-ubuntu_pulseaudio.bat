e9f339eff8f9f0bfb7ef1b37b13951822d6ef8c6
Vladimir Ovsyannikov <ov.vladimir@mail.ru>
2022-05-10 12:35:02 +1000

    Update start-ubuntu_pulseaudio.bat

--- start-ubuntu_pulseaudio.bat
+++ start-ubuntu_pulseaudio.bat
@@ -5,7 +5,7 @@ rem https://habr.com/ru/post/522726/
 start "" /B C:\Users\ovvla\Documents\Ubuntu\config.xlaunch
 start "" /B "C:\Users\ovvla\Documents\Ubuntu\pulseaudio\bin\pulseaudio.exe" --use-pid-file=false -D > nul
 
-ubuntu1804.exe run "cd ~ ; hostip=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}') ; export LIBGL_ALWAYS_INDIRECT=1 ; export DISPLAY=$hostip:0 ; export PULSE_SERVER=tcp:$hostip ; startlxde ; pkill '(gpg|ssh)-agent' ;"
+ubuntu1804.exe run "hostip=$(cat /etc/resolv.conf | grep nameserver | awk '{print $2}') ; export LIBGL_ALWAYS_INDIRECT=1 ; export DISPLAY=$hostip:0 ; export PULSE_SERVER=tcp:$hostip ; startlxde ; pkill '(gpg|ssh)-agent' ;"
 
 rem ubuntu1804.exe run "IP=$(host `hostname` | grep -oP '(\s)\d+(\.\d+){3}' | tail -1 | awk '{ print $NF }' | tr -d '\r') ; export LIBGL_ALWAYS_INDIRECT=Yes ; export NO_AT_BRIDGE=1 ; export DISPLAY=$IP:0.0 ; PULSE_SERVER=tcp:$IP ; startlxde ; pkill '(gpg|ssh)-agent' ;"
 
